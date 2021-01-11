7fd6d2ead0a0b4f733549a8ce048e57321c036d0
nguyenduongkhai <duongkhai.nguyen@my.jcu.edu.au>
2021-01-12 00:45:47 +0700

    first commit

---
+++ src/Model/SaveFile.java
@@ -0,0 +1,29 @@
+package Model;
+
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class SaveFile {
+    public static void main(String[] args) {
+        Path file = Paths.get("E:\\Task2\\src\\Model.SaveFile.txt");
+        String s = "LA";
+        byte[] data = s.getBytes();
+        OutputStream output;
+        try {
+            output = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            output.write(data);
+            output.flush();
+            output.close();
+        } catch (FileAlreadyExistsException e) {
+            System.out.println("File already exists");
+        } catch (Exception e) {
+            System.out.println("Message: " + e);
+        }
+    }
+}
